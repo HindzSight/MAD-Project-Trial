46f7491b4b4a99e17ee42ade069f8e2ce7b78bf4
Prince Rajpoot <44585452+princerajpoot20@users.noreply.github.com>
2023-05-17 13:19:35 +0530

    Added SplashScreen File
    Working on SplashScreen. Ver-4.1

--- app/src/main/java/com/example/paper/SplashScreen.java
+++ app/src/main/java/com/example/paper/SplashScreen.java
@@ -1,25 +1,25 @@
-package com.example.paper;
+// package com.example.paper;
 
-import androidx.appcompat.app.AppCompatActivity;
+// import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
+// import android.content.Intent;
+// import android.os.Bundle;
+// import android.os.Handler;
 
-public class SplashScreen extends AppCompatActivity {
+// public class SplashScreen extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-        getSupportActionBar().hide();
+//     @Override
+//     protected void onCreate(Bundle savedInstanceState) {
+//         super.onCreate(savedInstanceState);
+//         setContentView(R.layout.activity_splash_screen);
+//         getSupportActionBar().hide();
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-            }
-        },2000);
-    }
-}
+//         Handler handler = new Handler();
+//         handler.postDelayed(new Runnable() {
+//             @Override
+//             public void run() {
+//                 startActivity(new Intent(getApplicationContext(),MainActivity.class));
+//             }
+//         },2000);
+//     }
+// }
