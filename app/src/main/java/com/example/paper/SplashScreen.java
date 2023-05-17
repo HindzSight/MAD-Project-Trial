155b75c307a4e13f553c34ebadadb18159367805
Prince Rajpoot <44585452+princerajpoot20@users.noreply.github.com>
2023-05-17 13:23:13 +0530

    Working on SplashScreen.java. Ver-4.4
    Changes. Ver-4.4

--- app/src/main/java/com/example/paper/SplashScreen.java
+++ app/src/main/java/com/example/paper/SplashScreen.java
@@ -10,6 +10,10 @@ public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+
     }
 }
